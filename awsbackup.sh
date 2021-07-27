d3125c33d5935ee378932986d6c40f341932b5f2
Leander Seidlitz <leanderseidlitz@protonmail.com>
2021-07-27 18:28:11 +0200

    fix RSA encryption, misc cleanups

--- awsbackup.sh
+++ awsbackup.sh
@@ -8,22 +8,30 @@ BASEDIR="$(dirname $0)"
 source $BASEDIR/common/common.sh
 
 # load config file
-if test -f $BASEDIR/config ; then
-    . $BASEDIR/config
+if [[ -f "$BASEDIR/config" ]]
+then
+    . "$BASEDIR/config"
 else
     die "config file $BASEDIR/config missing"
 fi
 
 uploadClass () {
-    aws s3 cp $1 "s3://$awsbucket/$jobname/$2/" --storage-class $3
+    aws s3 cp "$1" "s3://$awsbucket/$jobname/$2/" --storage-class "$3"
 }
 upload () {
-    aws s3 cp $1 "s3://$awsbucket/$jobname/$2/"
+    aws s3 cp "$1" "s3://$awsbucket/$jobname/$2/"
 }
 
 log () {
-    echo "$(date "+%Y-%m-%dT%H:%M:%SZ") $1" >> $BASEDIR/backup.log
+    echo "$(date "+%Y-%m-%dT%H:%M:%SZ") $1" >> "$BASEDIR/backup.log"
     einfo "$1"
+}
+
+cleanup () {
+    if [[ -n "${jobdir:-''}" ]]
+    then
+	rm -r "$jobdir"
+    fi
 }
 
 printusage () {
@@ -36,7 +44,7 @@ printusage () {
 }
 
 checkkey () {
-    if [ ! -f "$pubkeyfile" ]
+    if [[ ! -f "$pubkeyfile" ]]
     then
 	log "Public keyfile ($pubkeyfile) missing"
 	countdown "Generating new keys in " 5
@@ -61,79 +69,92 @@ done
 shift $((OPTIND-1))
 
 jobname=$1
-if [ ! -f "$BASEDIR/jobs/$jobname" ]; then
+if [[ ! -f "$BASEDIR/jobs/$jobname" ]]
+then
     error "job file $BASEDIR/jobs/$jobname is missing."
     die "action: create jobfile at $BASEDIR/jobs/$jobname"
 fi
 
-if [ ! -d $(dirname "$tmpdir") ]
+if [[ ! -d "$(dirname "$tmpdir")" ]]
 then
     eerror "parent of tmpdir ($(dirname $tmpdir)) is missing."
     die "action: create dir, fix config"
 fi
 [[ -d "$tmpdir" ]] || mkdir -p "$tmpdir"
 
+trap cleanup EXIT
+
 # read file list to relative paths from /, space separated, escape spaces in filename
 files=""
 while read f
 do
     files="$files $(realpath --relative-to=/ "$(echo "$f" | sed 's/ /\\ /g')")"
-done < $BASEDIR/jobs/$jobname
+done < "$BASEDIR/jobs/$jobname"
 
 log "Backing up $files"
 # check that pubkey is present, otherwise generate
 checkkey
 
 # define filenames
-datetime=$(date "+%Y%m%dT%H%M%SZ")
-tarkey="$tmpdir/$jobname-$datetime.tarkey"
-listkey="$tmpdir/$jobname-$datetime.listkey"
+datetime="$(date "+%Y%m%dT%H%M%SZ")"
+jobdir="$tmpdir/$jobname-$datetime"
+[[ -d "$jobdir" ]] || mkdir -p "$jobdir"
+tarkey="$jobdir/$jobname-$datetime.tarkey"
+listkey="$jobdir/$jobname-$datetime.listkey"
+metakey="$jobdir/$jobname-$datetime.metakey"
 if [ "$zstd" == "true" ]; then
-    enctar="$tmpdir/$jobname-$datetime.tar.zstd.aes"
+    enctar="$jobdir/$jobname-$datetime.tar.zstd.aes"
     compopts="-I zstd"
 else
-    enctar="$tmpdir/$jobname-$datetime.tar.aes"
+    enctar="$jobdir/$jobname-$datetime.tar.aes"
     compopts=""
 fi
-enclist="$tmpdir/$jobname-$datetime.list.aes"
-meta="$tmpdir/$jobname-$datetime.meta"
-encmeta="$tmpdir/$jobname-$datetime.meta.aenc"
+enclist="$jobdir/$jobname-$datetime.list.aes"
+meta="$jobdir/$jobname-$datetime.meta"
+encmeta="$jobdir/$jobname-$datetime.meta.aes"
 
 log "Starting backup to AWS, jobname $jobname"
 
 log "Generating keys"
-openssl rand -hex 64 > $tarkey
-openssl rand -hex 64 > $listkey
+openssl rand -hex 64 > "$tarkey"
+openssl rand -hex 64 > "$listkey"
+openssl rand -hex 64 > "$metakey"
 
 # create the archive and file list
 log "Generating mtree file list $enclist"
-bsdtar -C / -cf - --format=mtree --options='sha256' $files | openssl aes-256-ctr -salt -pbkdf2 -pass file:$listkey -out $enclist
+bsdtar -C / -cf - --format=mtree --options='sha256' $files | openssl aes-256-ctr -salt -pbkdf2 -pass file:"$listkey" -out "$enclist"
 
 log "Backing up to encrypted $enctar"
-tar $compopts --warning=no-file-changed -cf - -C / $files | openssl aes-256-ctr -salt -pbkdf2 -pass file:$tarkey -out $enctar
+tar $compopts --warning=no-file-changed -cf - -C / $files | openssl aes-256-ctr -salt -pbkdf2 -pass file:"$tarkey" -out "$enctar"
 
 log "Generating metafile"
-echo "SHA256 LIST $(sha256sum $enclist)" > $meta
-echo "SHA256 TAR  $(sha256sum $enctar)" >> $meta
-echo "TARKEY      $(cat $tarkey)" >> $meta
-echo "LISTKEY     $(cat $listkey)" >> $meta
+cat > "$meta" <<EOF
+BKP VERSION $(git -C "$BASEDIR" rev-parse HEAD || echo "not git versioned")
+SHA256 LIST $(sha256sum $enclist)
+SHA256 TAR  $(sha256sum $enctar)
+TARKEY      $(cat $tarkey)
+LISTKEY     $(cat $listkey)
+EOF
 
 log "Encrypting metafile"
-openssl rsautl -encrypt -pubin -inkey $pubkeyfile -in $meta -out $encmeta
+openssl aes-256-ctr -salt -pbkdf2 -pass file:"$metakey" -out "$encmeta" < "$meta"
+openssl rsautl -encrypt -pubin -inkey "$pubkeyfile" -in "$metakey" -out "$metakey.aenc"
 
 # delete the tar
 log "Deleting unencrypted keys, metafile"
-rm -f $tarkey $listkey $meta
+shred -u "$tarkey" "$listkey" "$metakey" "$meta"
 
+log "Uploading encrypted metakey $metakey.aenc to AWS"
+upload "$metakey.aenc" "$datetime"
 log "Uploading $encmeta to AWS"
-upload $encmeta $datetime
+upload "$encmeta" "$datetime"
 log "Uploading $enclist to AWS"
-upload $enclist $datetime
+upload "$enclist" "$datetime"
 log "Uploading $enctar to AWS $storageclass"
-uploadClass $enctar $datetime $storageclass
+uploadClass "$enctar" "$datetime" "$storageclass"
 
 log "Deleting leftover files after upload"
-rm -f $encmeta $enclist $enctar
+rm -f "$encmeta" "$enclist" "$enctar"
 
 
 log "Done backing up to AWS"
